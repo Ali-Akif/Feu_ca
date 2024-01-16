f22a1253718404366a02cd4498695ffa99d9158f
Ali-Akif <ali-akif@live.fr>
2024-01-16 23:39:37 +0100

    Update feu00.py

--- feu00.py
+++ feu00.py
@@ -2,8 +2,6 @@
 
 import sys
 
-# Part 1 : function
-
 def rectangle_printer(width, height):
     top_and_bottom = "o" + "-" * (width - 2) + "o" if width > 1 else "o"
     middle = "|" + " " * (width - 2) + "|" if width > 1 else "|" 
@@ -15,15 +13,10 @@ def rectangle_printer(width, height):
             print(middle)
 
 
-# Part 2 : Error Handling and slicing
-
 if len(sys.argv) != 3 or not "".join(sys.argv[1:]).isdigit() or "0" in sys.argv[1:]:
     print("error")
     sys.exit(1)
 else:
     width, lenght = int(sys.argv[1]), int(sys.argv[2])
-
-
-# Part 3 : Resolution and display
 
 rectangle_printer(width, lenght)
