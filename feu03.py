aa057ffcd6344ab4d515df3cca6b9eee84aa4226
Ali-Akif <ali-akif@live.fr>
2024-01-22 17:00:15 +0100

    Update feu03.py

--- feu03.py
+++ feu03.py
@@ -1,18 +1,28 @@
 # Sudoku
 
+import sys
+
 def read_sudoku(file_path):
     with open(file_path, 'r') as file:
-        sudoku = [[int(num) if num != '.' else 0 for num in line] for line in file]
+        for row in file:
+            for char in row.strip():
+                if char != "." and not char.isdigit():
+                    print("Error in sudoku format")
+                    sys.exit(1)
+
+    with open(file_path, 'r') as file:
+        sudoku = [[int(num) if num != '.' else 0 for num in line.strip()] for line in file]
+
+    if len(sudoku) != 9 or not all(len(row) == 9 for row in sudoku):
+        print("Error: Sudoku grid should be 9x9")
+        sys.exit(1)
     return sudoku
 
 def is_valid(sudoku, row, col, num):
-    # Vérifier la ligne
     if num in sudoku[row]:
         return False
-    # Vérifier la colonne
     if num in [sudoku[i][col] for i in range(9)]:
         return False
-    # Vérifier le carré 3x3
     startRow, startCol = 3 * (row // 3), 3 * (col // 3)
     for i in range(startRow, startRow + 3):
         for j in range(startCol, startCol + 3):
@@ -21,10 +31,10 @@ def is_valid(sudoku, row, col, num):
     return True
 
 def solve_sudoku(sudoku):
-    empty = find_empty(sudoku)
+    empty = find_empty(sudoku) 
     if not empty:
         return True
-    row, col = empty
+    row, col = empty 
     for num in range(1, 10):
         if is_valid(sudoku, row, col, num):
             sudoku[row][col] = num
@@ -34,8 +44,8 @@ def solve_sudoku(sudoku):
     return False
 
 def find_empty(sudoku):
-    for i in range(9):
-        for j in range(9):
+    for i in range(9): 
+        for j in range(9): 
             if sudoku[i][j] == 0:
                 return i, j
     return None
@@ -51,9 +61,7 @@ def main(file_path):
     else:
         print("Pas de solution")
 
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1:
-        main(sys.argv[1])
-    else:
-        print("Usage: python sudoku_solver.py [file path]")
+if len(sys.argv) > 1:
+    main(sys.argv[1])
+else:
+    print("Usage: python sudoku_solver.py [file path]")
