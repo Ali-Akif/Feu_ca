c3eaef7f220ed399398b4bb31f59096f69ea86a3
Ali-Akif <ali-akif@live.fr>
2024-01-24 02:30:21 +0100

    Create feu06.py

---
+++ feu06.py
@@ -0,0 +1,3 @@
+# Feu OK
+
+print("J'ai terminé l'épreuve du feu, et c'était plus dur que ce à quoi je m'attendais, mais maintenant ça me parait plus simple qu'au début. Je sens mes progrès et je suis fier d'avoir fini les épreuves.")
