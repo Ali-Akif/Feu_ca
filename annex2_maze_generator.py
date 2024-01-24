cb69a2ea2bdb1a3fc7d7a901a636cc2886a34976
Ali-Akif <ali-akif@live.fr>
2024-01-24 01:58:00 +0100

    Update annex2_maze_generator.py

--- annex2_maze_generator.py
+++ annex2_maze_generator.py
@@ -9,13 +9,13 @@ def generate_maze(height, width, wall_char, path_char, start_char, end_char):
         row = ""
         for x in range(width):
             if y == height - 1 and x == entry:
-                row += start_char  # Départ
+                row += start_char
             elif y == 0 and x == exit:
-                row += end_char  # Arrivée
+                row += end_char
             elif 1 <= y < height - 1 and 1 <= x < width - 1 and random.randint(0, 99) > 205 :
-                row += path_char  # Case vide
+                row += path_char
             else:
-                row += wall_char  # Obstacle
+                row += wall_char
         maze.append(row)
     return maze
 
